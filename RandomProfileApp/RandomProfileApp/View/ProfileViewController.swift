d298b714a3426dac9b0d4a99bcc0c4270124e39f
Stepan Kirillov <stepakirillov59@gmail.com>
2022-01-12 13:01:48 +0300

    init progect and storyboard removed

---
+++ RandomProfileApp/RandomProfileApp/View/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomProfileApp
+//
+//  Created by Stepan Kirillov on 1/12/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .blue
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
